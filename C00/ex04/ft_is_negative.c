ac52542e9f16b8938bec8a75a7e796035a1321e6
kjroy93 <kmarrero1993@gmail.com>
2025-02-21 14:37:27 +0100

    modificacion de el ex04

--- C00/ex04/ft_is_negative.c
+++ C00/ex04/ft_is_negative.c
@@ -3,9 +3,9 @@
 void ft_is_negative(int n)
 {
     if (n >= 0)
-        write(1, "P", 2);  // Agrega un salto de línea
+        write(1, "P", 2);
     else
-        write(1, "N", 2);  // Agrega un salto de línea
+        write(1, "N", 2);
 }
 
 int main()
